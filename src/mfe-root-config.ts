dbbd3553daaab777968919712839ca1b8f19e745
Lucas Begnini <lucas.begnini@qyon.com>
2024-05-03 16:53:26 -0300

    chore: setup express

--- src/mfe-root-config.ts
+++ src/mfe-root-config.ts
@@ -2,19 +2,19 @@ import { registerApplication, start, LifeCycles } from "single-spa";
 
 registerApplication({
   name: "@mfe/navbar",
-  app: () => System.import<LifeCycles>("http://localhost:8081/mfe-navbar.js"),
+  app: () => System.import<LifeCycles>("@mfe/navbar"),
   activeWhen: [""],
 });
 
 registerApplication({
   name: "@mfe/react",
-  app: () => System.import<LifeCycles>("http://localhost:8080/mfe-newApp.js"),
+  app: () => System.import<LifeCycles>("@mfe/react"),
   activeWhen: ["/app", "/react"],
 });
 
 registerApplication({
   name: "@mfe/vueapp",
-  app: () => System.import<LifeCycles>("http://localhost:8082/js/app.js"),
+  app: () => System.import<LifeCycles>("@mfe/vueapp"),
   activeWhen: ["/app", "/vue"],
 });
 
